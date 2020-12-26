8e2d8414604561303cb3aeab46435e006058ce4f
imafikus <aleksatesicteske@gmail.com>
2020-12-26 17:44:39 +0100

    Remove unused functions from chat.ts

--- client/src/utils/chat.ts
+++ client/src/utils/chat.ts
@@ -1,35 +1,9 @@
 import {
-  ActiveUser,
   Chat
 } from '../types';
 
-const cmpUsers = (a: ActiveUser, b: ActiveUser): number => {
-  if (a.username < b.username) {
-    return -1
-  }
-  if (a.username > b.username) {
-    return 1
-  }
-  return 0
-} 
-
-function arraysAreEqual<T> (arr1: Array<T>, arr2: Array<T>): boolean {
-  
-  console.log('comparing: ', arr1);
-  console.log('comparing: ', arr2);
-  return JSON.stringify(arr1) === JSON.stringify(arr2);
-}
-
 const getSortedUsernamesFromChat = (chat: Chat): string[] => {
   return chat.users.map(user => user.username).sort();
-}
-
-export const prepareChatForSaving = (groupChat: Chat): Chat => {
-  const sortedUsers = groupChat.users.sort(cmpUsers)
-  return {
-    ...groupChat,
-    users: sortedUsers
-  }
 }
 
 export const chatAlreadyExists = (currentUserChats: Chat[], newChat: Chat): boolean => {
