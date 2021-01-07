493c20f5c733e0028add4569bf4e4c4eed8b7c20
imafikus <aleksatesicteske@gmail.com>
2021-01-07 21:46:19 +0100

    Remove unused env vars

--- server/src/envVal.ts
+++ server/src/envVal.ts
@@ -8,8 +8,7 @@ export interface EnvValType {
 
 const loadEnvVars = (): EnvValType => {
   if(
-    !process.env.SERVER_PORT || 
-    !process.env.SERVER_URL
+    !process.env.SERVER_PORT
     ) {
       throw new Error('Invalid .env file, some values are undefined, please check .env.example to see what you are missing');
   }
